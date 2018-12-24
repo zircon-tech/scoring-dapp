663b997253a317f0ea18ebcd1e0aa6d2a8c96a0d
Will Olivera <liamsolivera@gmail.com>
2018-12-24 16:28:12 -0300

    enhance: Receive ids from outside

--- contracts/Scoring.sol
+++ contracts/Scoring.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 contract Scoring {
 
-    event _newApp(bytes32 appKey, address sender);
+    event _newOrganization(bytes32 appKey, address sender);
     event _newScorer(bytes32 appKey, bytes32 scorerId, address sender);
     event _newResource(bytes32 appKey, bytes32 resourceId, address sender);
     event _newScore(bytes32 appKey, bytes32 scorerId, bytes32 resourceId, uint value);
@@ -41,17 +41,7 @@ contract Scoring {
         mapping (bytes32 => Score[]) scoresReceived; // Scores received by a resource
     }
 
-    uint keyGen;
     mapping (bytes32 => Organization) public organizations;
-
-    function _getHash() internal returns (bytes32) {
-        keyGen++;
-        return keccak256(abi.encodePacked(keyGen));
-    }
-
-    function getAppKey() public returns (bytes32) {
-        return _getHash();
-    }
 
     function _getTotalScore(bytes32 _appKey, bytes32 _resourceId, uint _value) internal view returns (uint) {
         return organizations[_appKey].resources[_resourceId].totalScore + _value;
@@ -70,34 +60,24 @@ contract Scoring {
         return ((average * total) + _value) / (total + 1);
     }
 
-    function registerApp(bytes32 _appKey, uint _min, uint _max) public {
+    function registerOrganization(bytes32 _appKey, uint _min, uint _max) public {
         organizations[_appKey].proof = true;
         organizations[_appKey].scoring = ScoringOptions(_min, _max);
-        emit _newApp(_appKey, msg.sender);
+        emit _newOrganization(_appKey, msg.sender);
     }
 
-    function registerScorer(bytes32 _appKey, string _metadata) public returns (bytes32) {
+    function registerScorer(bytes32 _appKey, bytes32 scorerId, string _metadata) public {
         require(organizations[_appKey].proof == true, "App not found");
 
-        bytes32 scorerId = _getHash();
-        organizations[_appKey].scorers[scorerId] = Scorer({ proof: true, metadata: _metadata });
+        organizations[_appKey].scorers[scorerId] = Scorer(true, _metadata);
         emit _newScorer(_appKey, scorerId, msg.sender);
-        return scorerId;
     }
 
-    function registerResource(bytes32 _appKey, string _metadata) public returns (bytes32) {
+    function registerResource(bytes32 _appKey, bytes32 resourceId, string _metadata) public returns (bytes32) {
         require(organizations[_appKey].proof == true, "App not found");
 
-        bytes32 resourceId = _getHash();
-        organizations[_appKey].resources[resourceId] = Resource({
-            proof: true,
-            metadata: _metadata,
-            totalScore: 0,
-            averageScore: 0,
-            overallScore: 0
-        });
+        organizations[_appKey].resources[resourceId] = Resource(true, _metadata, 0, 0, 0);
         emit _newResource(_appKey, resourceId, msg.sender);
-        return resourceId;
     }
 
     function sendScore(bytes32 _appKey, bytes32 _scorerId, bytes32 _resourceId, uint _value, uint _weight) public {
@@ -118,17 +98,12 @@ contract Scoring {
         emit _newScore(_appKey, _scorerId, _resourceId, _value * w);
     }
 
-    function getScore(bytes32 _appKey, bytes32 _resourceId, int _scoreType) public view returns (uint) {
+    function getScore(bytes32 _appKey, bytes32 _resourceId) public view returns (uint, uint, uint) {
         require(organizations[_appKey].proof == true, "App not found");
 
-        // scoreType 1 -> averageScore
-        // scoreType 2 -> overallScore
-        // scoreType n -> totalScore
-        if (_scoreType == 1) {
-            return organizations[_appKey].resources[_resourceId].averageScore;
-        } else if (_scoreType == 2) {
-            return organizations[_appKey].resources[_resourceId].overallScore;
-        }
-        return organizations[_appKey].resources[_resourceId].totalScore;
+        uint totalScore = organizations[_appKey].resources[_resourceId].totalScore;
+        uint averageScore = organizations[_appKey].resources[_resourceId].averageScore;
+        uint overallScore = organizations[_appKey].resources[_resourceId].overallScore;
+        return (totalScore, averageScore, overallScore);
     }
 }
